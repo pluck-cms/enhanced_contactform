06f5f81c727dd33d63425f178b61e3a44308c511
ekyo-pl <ekyo.serwis@gmail.com>
2018-07-14 13:29:57 +0200

    translation
    polish translation and some english lang fixes

---
+++ lang/pl.php
@@ -0,0 +1,33 @@
+<?php
+$lang['enhanced_contactform']['module_name']  = 'Ulepszony formularz kontaktowy';
+$lang['enhanced_contactform']['module_intro'] = 'Za pomocą tego formularza umożliwiasz odwiedzającym na wysyłanie wiadomości na różne adresy e-mail';
+$lang['enhanced_contactform']['fields']       = 'Nie wypełniłeś poprawnie wszystkich pól.';
+$lang['enhanced_contactform']['email_title']  = 'Wiadomość z Twojej strony od';
+$lang['enhanced_contactform']['been_send']    = 'Twoja wiadomość została wysłana poprawnie.';
+$lang['enhanced_contactform']['not_send']     = 'Twoja wiadomość nie mogła zostać wysłana, wystąpił błąd.';
+
+$lang['enhanced_contactform']['main']      = 'Zarządzaj ulepszonym formularzem kontaktowym';
+
+$lang['enhanced_contactform']['pagessite'] = 'Utwórz nowy wpis';
+$lang['enhanced_contactform']['adminpage'] = 'Aktywuj wpis';
+$lang['enhanced_contactform']['newentry']  = 'Utwórz nowy wpis';
+$lang['enhanced_contactform']['name']      = 'Imię';
+$lang['enhanced_contactform']['email']     = 'Adres e-mail';
+$lang['enhanced_contactform']['to']        = 'E-mail do';
+$lang['enhanced_contactform']['send']      = 'Wyślij';
+$lang['enhanced_contactform']['fillall']   = 'Nie wypełniłeś wszystkich pól.';
+$lang['enhanced_contactform']['nohtml']    = 'Tagi HTML nie są dozwolone';
+$lang['enhanced_contactform']['activate']  = 'Aktywuj wpis';
+$lang['enhanced_contactform']['delete']    = 'Usuń wpis';
+$lang['enhanced_contactform']['deleted']   = ' został usunięty z systemu.';
+$lang['enhanced_contactform']['backlink']  = 'Powrót na stronę główną';
+$lang['enhanced_contactform']['exhead']    = 'Zastrzeżenia';
+$lang['enhanced_contactform']['exredir']   = 'Zostaniesz przekierowany do';
+$lang['enhanced_contactform']['adminset']  = 'Ustawienia twojej księgi gości';
+$lang['enhanced_contactform']['pageis']    = 'Zrzeczenie się odpowiedzialności';
+$lang['enhanced_contactform']['settings']  = 'Ustawienia';
+$lang['enhanced_contactform']['save']      = 'Zapisz Ustawienia';
+$lang['enhanced_contactform']['distext']   = 'Tutaj wpisz swój tekst zastrzeżeń';
+$lang['enhanced_contactform']['captcha']   = 'Wpisz kod weryfikacyjny';
+$lang['enhanced_contactform']['notvalid']  = 'Kod weryfikacyjny nie został wprowadzony poprawnie';
+?>
